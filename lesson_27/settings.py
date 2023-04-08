e1e135241e4ca6d5ed1806e9844a21bf53a29f58
mkN <mknord1565@gmail.com>
2023-04-08 10:50:31 +0600

    add data manager

--- lesson_27/settings.py
+++ lesson_27/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,8 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1200),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
+}
